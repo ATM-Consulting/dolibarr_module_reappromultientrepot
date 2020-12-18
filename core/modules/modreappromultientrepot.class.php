3a33e8f121495d4f2aadd0bec5d55fa791317ee6
boudet jean pascal <68547382+atm-jpb@users.noreply.github.com>
2020-12-18 12:13:00 +0100

    Update modreappromultientrepot.class.php
    remove user rights

--- core/modules/modreappromultientrepot.class.php
+++ core/modules/modreappromultientrepot.class.php
@@ -177,7 +177,7 @@ class modreappromultientrepot extends DolibarrModules
 		// Example:
 		$this->rights[$r][0] = $this->numero . $r;	// Permission id (must not be already used)
 		$this->rights[$r][1] = 'Imprimer documents de réapprovisionnement';	// Permission label
-		$this->rights[$r][3] = 1; 					// Permission by default for new user (0/1)
+		$this->rights[$r][3] = 0; 					// Permission by default for new user (0/1)
 		$this->rights[$r][4] = 'lire';				// In php code, permission will be checked by test if ($user->rights->permkey->level1->level2)
 		$r++;
 
